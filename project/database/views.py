edf06a3df7c97b4c203c04cd32db9cf72004bb3e
quddus2 <quddus2@illinois.edu>
2018-04-20 22:11:10 -0500

    Hell Yeah Baby

--- project/database/views.py
+++ project/database/views.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django import template
+from django.template.loader import get_template 
+from database.models import Question
+
+def search(request):
+    template = get_template('search.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def results(request):
+	query = request.GET['q']
+	template = get_template('results.html')
+
+	try:
+		results=Question.objects.filter(question_text= query)
+		context = { 'results': results}
+	except Question.DoesNotExist:
+		results="wrong"
+		context = { 'results': results}	
+
+	return HttpResponse(template.render(context,request))
